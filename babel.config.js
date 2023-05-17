cefc961c37848043e4cbacdeb3dc670f69a68a0b
SangYoonLee <prestro1231@hufs.ac.kr>
2023-05-17 20:06:18 +0900

    Add: 프로젝트 CRA 기본 세팅

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+          chrome: '79',
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ],
+};
