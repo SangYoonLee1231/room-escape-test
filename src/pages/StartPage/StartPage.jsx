0497aaad910f72570f5188b380a695e0fc81739b
SangYoonLee <prestro1231@hufs.ac.kr>
2023-06-04 22:51:47 +0900

    Add: 응시자 수 추가

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -14,7 +14,7 @@ const StartPage = () => {
       <SubTitle>
         <Text>방탈출 초보부터 고수까지</Text>
         <Text>모두를 위한 방탈출 능력 미니 테스트</Text>
-        <Text>(응시생 수 : 1046명)</Text>
+        <Text>(총 응시생 수 : 1046명 이상)</Text>
         {/* <Text>Made by SYL</Text> */}
       </SubTitle>
       <StartButton onClick={onAboutClick}>START</StartButton>
@@ -68,7 +68,7 @@ const SubTitle = styled.div`
   justify-content: center;
   align-items: center;
   width: 280px;
-  height: 120px;
+  height: 125px;
   margin: 4vh 0vh;
   border: 3px solid white;
   border-radius: 20px;
