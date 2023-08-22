554f346c7de40065b60c5c7ef258c99ff801dbe1
SangYoonLee <prestro1231@hufs.ac.kr>
2023-08-22 21:15:31 +0900

    Add: 결과 페이지에 광고 코드 추가

--- src/pages/TestOnePage/TestOneResultPage.jsx
+++ src/pages/TestOnePage/TestOneResultPage.jsx
@@ -99,150 +99,153 @@ const TestOneResultPage = () => {
   };
 
   return (
-    <MainBody>
-      <HeaderArea>
-        <Timer>00:00</Timer>
-      </HeaderArea>
-      {/* <Instruction src="./images/etc/before-start-test.png" /> */}
-      <Instruction height="1470px">
-        <InstructionTitle>▶︎ 테스트 결과 ◀︎</InstructionTitle>
-        <InstructionList />
-        <ul>
-          <InstructionList fontSize="50px">👏🏻</InstructionList>
+    <>
+      <GoogleAd {...infeedProps} />
+      <MainBody>
+        <HeaderArea>
+          <Timer>00:00</Timer>
+        </HeaderArea>
+        {/* <Instruction src="./images/etc/before-start-test.png" /> */}
+        <Instruction height="1470px">
+          <InstructionTitle>▶︎ 테스트 결과 ◀︎</InstructionTitle>
           <InstructionList />
-          <InstructionList fontSize="17px">수고하셨습니다.</InstructionList>
-          <InstructionList fontSize="17px">
-            {nickname}님의 '기초 역량 테스트' 점수는 {testScore}
-            점입니다.
-          </InstructionList>
-          <InstructionList fontSize="17px">
-            등급은 {testRank}입니다.
+          <ul>
+            <InstructionList fontSize="50px">👏🏻</InstructionList>
+            <InstructionList />
+            <InstructionList fontSize="17px">수고하셨습니다.</InstructionList>
+            <InstructionList fontSize="17px">
+              {nickname}님의 '기초 역량 테스트' 점수는 {testScore}
+              점입니다.
+            </InstructionList>
+            <InstructionList fontSize="17px">
+              등급은 {testRank}입니다.
+            </InstructionList>
+            <InstructionList>
+              <InstructionList />
+              <InstructionTitle>■ 채점표 ■</InstructionTitle>
+              <ResultTable>
+                <th className="short">번호</th>
+                <th className="long">제출한 답</th>
+                <th className="long">정답</th>
+                <th className="short">정답 여부</th>
+                {answersheet.map((value, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{value}</td>
+                    <td>{answer[index]}</td>
+                    <td>{value.toUpperCase() === answer[index] ? 'O' : 'X'}</td>
+                  </tr>
+                ))}
+              </ResultTable>
+              <InstructionTitle>■ '기초 역량 테스트' 등급표 ■</InstructionTitle>
+              <RankTable>
+                <th>등급</th>
+                <th>점수</th>
+                <th>평가</th>
+                {rank.map((value, index) => (
+                  <tr key={index}>
+                    <td>{value[0]}</td>
+                    <td>{value[1]}</td>
+                    <td>{value[2]}</td>
+                  </tr>
+                ))}
+              </RankTable>
+            </InstructionList>
+          </ul>
+          <InstructionList />
+          <InstructionList />
+        </Instruction>
+        <Instruction height="1000px" fontColor="black">
+          <InstructionList>
+            문제의 풀이가 궁금하신 분들을 위해 해설 답안을 제공합니다.
           </InstructionList>
           <InstructionList>
-            <InstructionList />
-            <InstructionTitle>■ 채점표 ■</InstructionTitle>
-            <ResultTable>
-              <th className="short">번호</th>
-              <th className="long">제출한 답</th>
-              <th className="long">정답</th>
-              <th className="short">정답 여부</th>
-              {answersheet.map((value, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{value}</td>
-                  <td>{answer[index]}</td>
-                  <td>{value.toUpperCase() === answer[index] ? 'O' : 'X'}</td>
-                </tr>
-              ))}
-            </ResultTable>
-            <InstructionTitle>■ '기초 역량 테스트' 등급표 ■</InstructionTitle>
-            <RankTable>
-              <th>등급</th>
-              <th>점수</th>
-              <th>평가</th>
-              {rank.map((value, index) => (
-                <tr key={index}>
-                  <td>{value[0]}</td>
-                  <td>{value[1]}</td>
-                  <td>{value[2]}</td>
-                </tr>
-              ))}
-            </RankTable>
+            (해설 사이트 접속까지 시간이 꽤 소요될 수 있습니다.)
           </InstructionList>
-        </ul>
-        <InstructionList />
-        <InstructionList />
-      </Instruction>
-      <Instruction height="1000px" fontColor="black">
-        <InstructionList>
-          문제의 풀이가 궁금하신 분들을 위해 해설 답안을 제공합니다.
-        </InstructionList>
-        <InstructionList>
-          (해설 사이트 접속까지 시간이 꽤 소요될 수 있습니다.)
-        </InstructionList>
-        <ExplanationBtn
-          onClick={() => {
-            window.open(
-              'https://superb-ranunculus-46a.notion.site/913536920aca4b0d96804336d2cc85a1'
-            );
-          }}
-          target="_blank"
-        >
-          해설 보러 가기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>
-          혹시 시험에 관하여 제작자에게 문의드리고 싶은 것이 있다면
-        </InstructionList>
-        <InstructionList>
-          일대일 오픈채팅방을 이용해주시기를 바랍니다.
-        </InstructionList>
-        <ExplanationBtn
-          onClick={() => {
-            window.open('https://open.kakao.com/o/sAFqQtae');
-          }}
-          target="_blank"
-        >
-          오픈채팅방 바로가기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>
-          또한, 제작자의 다른 작품을 플레이해보고 싶거나
-        </InstructionList>
-        <InstructionList>
-          제작자를 후원하고 싶으신 분들은 아래 버튼을 클릭해주세요.
-        </InstructionList>
-        <InstructionList />
-        <ExplanationBtn
-          onClick={() => {
-            window.open(
-              'https://superb-ranunculus-46a.notion.site/cb0658883e874aa6b9414d8da7c8aefa'
-            );
-          }}
-          target="_blank"
-        >
-          제작자의 다른 작품 보러가기
-        </ExplanationBtn>
-        <ExplanationBtn
-          onClick={() => {
-            window.open('https://toss.me/syl1231');
-          }}
-          target="_blank"
-        >
-          제작자 후원하기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList>
-          여러분의 후원은 제가 더 좋은 작품을 만드는 동기로 삼겠습니다.
-        </InstructionList>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>◆ 플레이 해주셔서 감사드립니다. ◆</InstructionList>
-        <InstructionList />
-      </Instruction>
-      <FooterArea>
-        {/* <NicknameBar>
+          <ExplanationBtn
+            onClick={() => {
+              window.open(
+                'https://superb-ranunculus-46a.notion.site/913536920aca4b0d96804336d2cc85a1'
+              );
+            }}
+            target="_blank"
+          >
+            해설 보러 가기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>
+            혹시 시험에 관하여 제작자에게 문의드리고 싶은 것이 있다면
+          </InstructionList>
+          <InstructionList>
+            일대일 오픈채팅방을 이용해주시기를 바랍니다.
+          </InstructionList>
+          <ExplanationBtn
+            onClick={() => {
+              window.open('https://open.kakao.com/o/sAFqQtae');
+            }}
+            target="_blank"
+          >
+            오픈채팅방 바로가기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>
+            또한, 제작자의 다른 작품을 플레이해보고 싶거나
+          </InstructionList>
+          <InstructionList>
+            제작자를 후원하고 싶으신 분들은 아래 버튼을 클릭해주세요.
+          </InstructionList>
+          <InstructionList />
+          <ExplanationBtn
+            onClick={() => {
+              window.open(
+                'https://superb-ranunculus-46a.notion.site/cb0658883e874aa6b9414d8da7c8aefa'
+              );
+            }}
+            target="_blank"
+          >
+            제작자의 다른 작품 보러가기
+          </ExplanationBtn>
+          <ExplanationBtn
+            onClick={() => {
+              window.open('https://toss.me/syl1231');
+            }}
+            target="_blank"
+          >
+            제작자 후원하기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList>
+            여러분의 후원은 제가 더 좋은 작품을 만드는 동기로 삼겠습니다.
+          </InstructionList>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>◆ 플레이 해주셔서 감사드립니다. ◆</InstructionList>
+          <InstructionList />
+        </Instruction>
+        <FooterArea>
+          {/* <NicknameBar>
           <NicknameSpan>테스트에 응해주셔서 감사드립니다</NicknameSpan>
           <NickNameInput onChange={event => handleChange(event)} />
         </NicknameBar> */}
-        <BtnArea>
-          <GoToFirstBtn onClick={moveToStartPage}>
-            처음으로 돌아가기
-          </GoToFirstBtn>
-        </BtnArea>
-      </FooterArea>
-    </MainBody>
+          <BtnArea>
+            <GoToFirstBtn onClick={moveToStartPage}>
+              처음으로 돌아가기
+            </GoToFirstBtn>
+          </BtnArea>
+        </FooterArea>
+      </MainBody>
+    </>
   );
 };
 
--- src/pages/TestThreePage/TestThreeResultPage.jsx
+++ src/pages/TestThreePage/TestThreeResultPage.jsx
@@ -104,62 +104,64 @@ const TestThreeResultPage = () => {
   };
 
   return (
-    <MainBody>
-      <HeaderArea>
-        <Timer>00:00</Timer>
-      </HeaderArea>
-      {/* <Instruction src="./images/etc/before-start-test.png" /> */}
-      <Instruction height="1420px">
-        <InstructionTitle>▶︎ 테스트 결과 ◀︎</InstructionTitle>
-        <InstructionList />
-        <ul>
-          <InstructionList fontSize="50px">👏🏻</InstructionList>
+    <>
+      <GoogleAd {...infeedProps} />
+      <MainBody>
+        <HeaderArea>
+          <Timer>00:00</Timer>
+        </HeaderArea>
+        {/* <Instruction src="./images/etc/before-start-test.png" /> */}
+        <Instruction height="1420px">
+          <InstructionTitle>▶︎ 테스트 결과 ◀︎</InstructionTitle>
           <InstructionList />
-          <InstructionList fontSize="17px">수고하셨습니다.</InstructionList>
-          <InstructionList fontSize="17px">
-            {nickname}님의 '고수 테스트' 점수는 {testScore}
-            점입니다.
-          </InstructionList>
-          <InstructionList fontSize="17px">
-            등급은 {testRank}입니다.
-          </InstructionList>
-          <InstructionList>
+          <ul>
+            <InstructionList fontSize="50px">👏🏻</InstructionList>
             <InstructionList />
-            <InstructionTitle>■ 채점표 ■</InstructionTitle>
-            <ResultTable>
-              <th className="short">번호</th>
-              <th className="long">제출한 답</th>
-              <th className="long">정답</th>
-              <th className="short">정답 여부</th>
-              {answersheet.map((value, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{value}</td>
-                  <td>{answer[index]}</td>
-                  <td>
-                    {value.split(' ').join('').toUpperCase() ===
-                    answer[index].toUpperCase()
-                      ? 'O'
-                      : 'X'}
-                  </td>
-                </tr>
-              ))}
-            </ResultTable>
-            <InstructionTitle>■ '고수 테스트' 등급표 ■</InstructionTitle>
-            <RankTable>
-              <th>등급</th>
-              <th>점수</th>
-              <th>평가</th>
-              {rank.map((value, index) => (
-                <tr key={index}>
-                  <td>{value[0]}</td>
-                  <td>{value[1]}</td>
-                  <td>{value[2]}</td>
-                </tr>
-              ))}
-            </RankTable>
-          </InstructionList>
-          {/* <InstructionList>
+            <InstructionList fontSize="17px">수고하셨습니다.</InstructionList>
+            <InstructionList fontSize="17px">
+              {nickname}님의 '고수 테스트' 점수는 {testScore}
+              점입니다.
+            </InstructionList>
+            <InstructionList fontSize="17px">
+              등급은 {testRank}입니다.
+            </InstructionList>
+            <InstructionList>
+              <InstructionList />
+              <InstructionTitle>■ 채점표 ■</InstructionTitle>
+              <ResultTable>
+                <th className="short">번호</th>
+                <th className="long">제출한 답</th>
+                <th className="long">정답</th>
+                <th className="short">정답 여부</th>
+                {answersheet.map((value, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{value}</td>
+                    <td>{answer[index]}</td>
+                    <td>
+                      {value.split(' ').join('').toUpperCase() ===
+                      answer[index].toUpperCase()
+                        ? 'O'
+                        : 'X'}
+                    </td>
+                  </tr>
+                ))}
+              </ResultTable>
+              <InstructionTitle>■ '고수 테스트' 등급표 ■</InstructionTitle>
+              <RankTable>
+                <th>등급</th>
+                <th>점수</th>
+                <th>평가</th>
+                {rank.map((value, index) => (
+                  <tr key={index}>
+                    <td>{value[0]}</td>
+                    <td>{value[1]}</td>
+                    <td>{value[2]}</td>
+                  </tr>
+                ))}
+              </RankTable>
+            </InstructionList>
+            {/* <InstructionList>
             ⁃ 답안지를 제출하거나 제한시간이 모두 지나면, 자동으로 채점되어 시험
             결과가 발표됩니다. 채점 시 영어 대소문자는 구분하지 않습니다.
           </InstructionList>
@@ -172,100 +174,101 @@ const TestThreeResultPage = () => {
             않도록 각별히 주의해주시기 바랍니다. 답안지가 모두 초기화될 수
             있습니다.
           </InstructionList> */}
-        </ul>
-        <InstructionList />
-        <InstructionList />
-      </Instruction>
-      <Instruction height="1000px" fontColor="black">
-        <InstructionList>
-          문제의 풀이가 궁금하신 분들을 위해 해설 답안을 제공합니다.
-        </InstructionList>
-        <InstructionList>
-          (해설 사이트 접속까지 시간이 꽤 소요될 수 있습니다.)
-        </InstructionList>
-        <ExplanationBtn
-          onClick={() => {
-            window.open(
-              'https://superb-ranunculus-46a.notion.site/38a27b0ee9104ad99fd01ac1f5c341a5?pvs=4'
-            );
-          }}
-          target="_blank"
-        >
-          해설 보러 가기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>
-          또한, 그 밖에도 제작자에게 문의드리고 싶은 것이 있으시다면
-        </InstructionList>
-        <InstructionList>
-          언제든 일대일 오픈채팅방을 이용해주시기 바랍니다.
-        </InstructionList>
-        <ExplanationBtn
-          onClick={() => {
-            window.open('https://open.kakao.com/o/sAFqQtae');
-          }}
-          target="_blank"
-        >
-          오픈채팅방 바로가기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>
-          또한, 제작자의 다른 작품을 플레이해보고 싶거나
-        </InstructionList>
-        <InstructionList>
-          제작자를 후원하고 싶으신 분들은 아래 버튼을 클릭해주세요.
-        </InstructionList>
-        <InstructionList />
-        <ExplanationBtn
-          onClick={() => {
-            window.open(
-              'https://superb-ranunculus-46a.notion.site/cb0658883e874aa6b9414d8da7c8aefa'
-            );
-          }}
-          target="_blank"
-        >
-          제작자의 다른 작품 보러가기
-        </ExplanationBtn>
-        <ExplanationBtn
-          onClick={() => {
-            window.open('https://toss.me/syl1231');
-          }}
-          target="_blank"
-        >
-          제작자 후원하기
-        </ExplanationBtn>
-        <InstructionList />
-        <InstructionList>
-          여러분의 후원은 제가 더 좋은 작품을 만드는 동기로 삼겠습니다.
-        </InstructionList>
-        <InstructionList />
-        <InstructionList />
-        <hr width="400px" />
-        <InstructionList />
-        <InstructionList />
-        <InstructionList>◆ 플레이 해주셔서 감사드립니다. ◆</InstructionList>
-        <InstructionList />
-      </Instruction>
-      <FooterArea>
-        {/* <NicknameBar>
+          </ul>
+          <InstructionList />
+          <InstructionList />
+        </Instruction>
+        <Instruction height="1000px" fontColor="black">
+          <InstructionList>
+            문제의 풀이가 궁금하신 분들을 위해 해설 답안을 제공합니다.
+          </InstructionList>
+          <InstructionList>
+            (해설 사이트 접속까지 시간이 꽤 소요될 수 있습니다.)
+          </InstructionList>
+          <ExplanationBtn
+            onClick={() => {
+              window.open(
+                'https://superb-ranunculus-46a.notion.site/38a27b0ee9104ad99fd01ac1f5c341a5?pvs=4'
+              );
+            }}
+            target="_blank"
+          >
+            해설 보러 가기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>
+            또한, 그 밖에도 제작자에게 문의드리고 싶은 것이 있으시다면
+          </InstructionList>
+          <InstructionList>
+            언제든 일대일 오픈채팅방을 이용해주시기 바랍니다.
+          </InstructionList>
+          <ExplanationBtn
+            onClick={() => {
+              window.open('https://open.kakao.com/o/sAFqQtae');
+            }}
+            target="_blank"
+          >
+            오픈채팅방 바로가기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>
+            또한, 제작자의 다른 작품을 플레이해보고 싶거나
+          </InstructionList>
+          <InstructionList>
+            제작자를 후원하고 싶으신 분들은 아래 버튼을 클릭해주세요.
+          </InstructionList>
+          <InstructionList />
+          <ExplanationBtn
+            onClick={() => {
+              window.open(
+                'https://superb-ranunculus-46a.notion.site/cb0658883e874aa6b9414d8da7c8aefa'
+              );
+            }}
+            target="_blank"
+          >
+            제작자의 다른 작품 보러가기
+          </ExplanationBtn>
+          <ExplanationBtn
+            onClick={() => {
+              window.open('https://toss.me/syl1231');
+            }}
+            target="_blank"
+          >
+            제작자 후원하기
+          </ExplanationBtn>
+          <InstructionList />
+          <InstructionList>
+            여러분의 후원은 제가 더 좋은 작품을 만드는 동기로 삼겠습니다.
+          </InstructionList>
+          <InstructionList />
+          <InstructionList />
+          <hr width="400px" />
+          <InstructionList />
+          <InstructionList />
+          <InstructionList>◆ 플레이 해주셔서 감사드립니다. ◆</InstructionList>
+          <InstructionList />
+        </Instruction>
+        <FooterArea>
+          {/* <NicknameBar>
           <NicknameSpan>테스트에 응해주셔서 감사드립니다</NicknameSpan>
           <NickNameInput onChange={event => handleChange(event)} />
         </NicknameBar> */}
-        <BtnArea>
-          <GoToFirstBtn onClick={moveToStartPage}>
-            처음으로 돌아가기
-          </GoToFirstBtn>
-        </BtnArea>
-      </FooterArea>
-    </MainBody>
+          <BtnArea>
+            <GoToFirstBtn onClick={moveToStartPage}>
+              처음으로 돌아가기
+            </GoToFirstBtn>
+          </BtnArea>
+        </FooterArea>
+      </MainBody>
+    </>
   );
 };
 
