f641a26b7c87bb746abbcd49940fb0bd3342d498
SangYoonLee <prestro1231@hufs.ac.kr>
2023-12-18 01:25:37 +0900

    Modify: 방문자 수 업데이트

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -17,7 +17,7 @@ const StartPage = () => {
         {/* <Text>Made by SYL</Text> */}
       </SubTitle>
       <SubTitleTwo>
-        <Text fontSize="small">▶︎ 방문자 수 : 9013명 ◀︎</Text>
+        <Text fontSize="small">▶︎ 방문자 수 : 9241명 ◀︎</Text>
         <Text fontSize="x-small">
           (방문자 수는 매주 월요일에 업데이트 됩니다)
         </Text>
