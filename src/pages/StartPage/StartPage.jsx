907700a1406da50def6258e7ffb366b591c9ed97
SangYoonLee <prestro1231@hufs.ac.kr>
2024-05-09 15:55:22 +0900

    Modify: 누적 응시자 수 업데이트

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -17,7 +17,7 @@ const StartPage = () => {
         {/* <Text>Made by SYL</Text> */}
       </SubTitle>
       <SubTitleTwo>
-        <Text fontSize="small">▶︎ 누적 방문자 수 : 14392명 ◀︎</Text>
+        <Text fontSize="small">▶︎ 누적 방문자 수 : 14855명 ◀︎</Text>
         <Text fontSize="x-small">
           (방문자 수는 매주 월요일에 업데이트 됩니다)
         </Text>
